fc51f9502a8e0a6b86fc721f4b635309b80b0717
OtavioTerra <otavioterraroque@gmail.com>
2022-02-18 17:13:44 -0300

    Add 3 pastas com os primeiros desafios de código do bootcamp

---
+++ Desafio 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int inicio = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"{inicio} {inicio*inicio} {inicio*inicio*inicio}");
+                inicio++;
+                
+            }
+        }
+    }
+}
---
+++ Desafio 3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int resultadoSoma = 0;
+
+            //Digite o seu código aqui
+            while(x != 0){
+                if(x % 2 == 0){
+                    for(int i = 0; i < 5; i++){
+                        resultadoSoma = resultadoSoma + x;
+                        x = x + 2;
+                    }
+                }
+                else{
+                    x = x+1;
+                    for(int i = 0; i < 5; i++){
+                        resultadoSoma = resultadoSoma + x;
+                        x = x + 2;
+                    }
+                }
+                Console.WriteLine(resultadoSoma);
+                x = int.Parse(Console.ReadLine());
+                resultadoSoma = 0;
+            }
+
+            }
+        }
+    }
